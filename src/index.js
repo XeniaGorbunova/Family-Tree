ec3aaf86e8d13228f0e91776167fb062f629100e
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-03-09 17:04:54 +0300

    add some pages and redux folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,78 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import { Provider } from 'react-redux/es/exports'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+import Main from './components/Main/Main'
+import SignIn from './components/SignIn/SignIn'
+import SignUp from './components/SignUp/SignUp'
+import './index.css'
+import Cart from './components/Cart/Cart'
+import { store } from './redux/store'
+import Cathalog from './components/Cathalog/Cathalog'
+import { PageNotFound } from './components/PageNotFound/PageNotFound'
+import DetailPage from './components/DatailPage/DetailPage'
+import UserPage from './components/UserPage/UserPage'
+import FavoritesPage from './components/Favorites/Favorites'
+// import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/signin',
+        element: <SignIn />,
+      },
+      {
+        path: '/products',
+        element: <Cathalog />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/product/:id',
+        element: <DetailPage />,
+      },
+      {
+        path: '/user/:id',
+        element: <UserPage />,
+      },
+      {
+        path: '/favorites',
+        element: <FavoritesPage />,
+      },
+      {
+        path: '*',
+        element: <PageNotFound />,
+      },
+    ],
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+
   </React.StrictMode>,
 )
