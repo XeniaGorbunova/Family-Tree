52afb84e38c5022834af2e7d5cf21e06e0bcb84d
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-03-10 23:57:33 +0300

    add styles

--- src/components/LocationPage/LocationPage.jsx
+++ src/components/LocationPage/LocationPage.jsx
@@ -4,7 +4,7 @@ import lineBottom from '../../assets/lineBottom.png'
 
 function LocationPage() {
   return (
-    <Container className="p-3" fluid>
+    <Container className="p-3 d-flex flex-column align-items-center" fluid>
       <h1 style={{ textAlign: 'center', fontWeight: '400' }} className="m-5">
         НАЙДИ ОТКУДА ТЫ РОДОМ
       </h1>
@@ -20,7 +20,7 @@ function LocationPage() {
           <Placemark defaultGeometry={[55.75, 37.57]} />
         </Map>
       </YMaps>
-      <p className="mb-4 mt-5">
+      <p className="mb-4 mt-5" style={{ maxWidth: '950px', textAlign: 'center' }}>
         Карта мира с отмеченными кородами, где родились члены вашей семьи.Несколько еще строчек,
         пока не придумала каких.
         Несколько еще строчек, пока не придумала каких.
