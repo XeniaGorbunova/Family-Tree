d6c6b256e9f7f782608615376166e8957b2d1648
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-03-10 12:02:00 +0300

    add maps

--- src/components/LocationPage/LocationPage.jsx
+++ src/components/LocationPage/LocationPage.jsx
@@ -1,5 +1,18 @@
+import { YMaps, Map } from 'react-yandex-maps'
+
 function LocationPage() {
-  return <h1>LocationPage</h1>
+  return (
+    <Container className="p-3">
+      <Jumbotron>
+        <h1>НАЙДИ ОТКУДА ТЫ РОДОМ</h1>
+        <YMaps>
+          <div>
+            <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+          </div>
+        </YMaps>
+      </Jumbotron>
+    </Container>
+  )
 }
 
 export default LocationPage
