8c1119f6e4c4a73eff45321bd144f29c8b4a012d
Liubov Gayduk <serenity0506@yandex.ru>
2023-03-11 00:04:45 +0300

    add Header JSX Layout and Styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom'
+import './HeaderStyles.css'
+import { MenuBar } from './MenuBar'
+
 function Header() {
-  return <h1>Header</h1>
+  console.log(1)
+  return (
+    <div className='bg'>
+      <div className='d-flex justify-content-md-between'>
+        <Link to='/' className='link'>
+          <span>О ПРОЕКТЕ</span>
+        </Link>
+        <MenuBar />
+      </div>
+    </div>
+  )
 }
 
 export default Header
---
+++ src/components/Header/MenuBar.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './HeaderStyles.css'
+
+export function MenuBar() {
+  const [token] = useState()
+
+  if (!token) {
+    return (
+      <Link to='signin' className='link'>
+        <span>ВОЙТИ / РЕГИСТРАЦИЯ</span>
+      </Link>
+    )
+  }
+
+  return (
+    <>
+      <input
+        className='search'
+        type='search'
+        placeholder='ПОИСК'
+        aria-label='Search'
+      />
+      <Link to='familytree' className='link'>
+        <span>СЕМЕЙНОЕ ДЕРЕВО</span>
+      </Link>
+
+      <Link to='location' className='link'>
+        <span>КАРТА</span>
+      </Link>
+
+      <Link to='/user/:id' className='link'>
+        <span>ЛИЧНЫЙ КАБИНЕТ</span>
+      </Link>
+    </>
+  )
+}
