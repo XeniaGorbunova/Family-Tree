901cc630b23844e5db8e9de046f4c70b43466049
robin3010 <robin3010@gmail.com>
2023-03-11 01:27:53 +0300

    added signIn/signUp pages content; combined singIn/signUp pages to LoginPages folder, added temporary logIn reducer; added login form and form components (alert, input)

---
+++ src/components/Forms/Errors/FetchErrorAlert.jsx
@@ -0,0 +1,12 @@
+import Alert from 'react-bootstrap/Alert'
+import './FetchErrorAlert.css'
+
+export function FetchErrorAlert({ isError, error }) {
+  if (isError) {
+    return (
+      <Alert variant="danger" className="fetch-error__alert">
+        {error}
+      </Alert>
+    )
+  }
+}
