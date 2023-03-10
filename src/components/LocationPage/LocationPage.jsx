c9d1361ad725c9856be9e7a0a17b3acb40445816
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-03-10 23:52:27 +0300

    add location page parts

--- src/components/LocationPage/LocationPage.jsx
+++ src/components/LocationPage/LocationPage.jsx
@@ -1,10 +1,13 @@
 import Container from 'react-bootstrap/Container'
 import { YMaps, Map, Placemark } from 'react-yandex-maps'
+import lineBottom from '../../assets/lineBottom.png'
 
 function LocationPage() {
   return (
     <Container className="p-3" fluid>
-      <h1 style={{ textAlign: 'center' }} className="m-5">НАЙДИ ОТКУДА ТЫ РОДОМ</h1>
+      <h1 style={{ textAlign: 'center', fontWeight: '400' }} className="m-5">
+        НАЙДИ ОТКУДА ТЫ РОДОМ
+      </h1>
       <YMaps style={{ width: '100%' }}>
         <Map
           defaultState={{
@@ -17,6 +20,13 @@ function LocationPage() {
           <Placemark defaultGeometry={[55.75, 37.57]} />
         </Map>
       </YMaps>
+      <p className="mb-4 mt-5">
+        Карта мира с отмеченными кородами, где родились члены вашей семьи.Несколько еще строчек,
+        пока не придумала каких.
+        Несколько еще строчек, пока не придумала каких.
+
+      </p>
+      <img src={lineBottom} alt="line" />
     </Container>
   )
 }
