131bdd288d38b206d8ffe9ae2175cf6b38accac1
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-03-10 19:38:49 +0300

    add yandex map

--- src/components/LocationPage/LocationPage.jsx
+++ src/components/LocationPage/LocationPage.jsx
@@ -1,16 +1,22 @@
-import { YMaps, Map } from 'react-yandex-maps'
+import Container from 'react-bootstrap/Container'
+import { YMaps, Map, Placemark } from 'react-yandex-maps'
 
 function LocationPage() {
   return (
-    <Container className="p-3">
-      <Jumbotron>
-        <h1>НАЙДИ ОТКУДА ТЫ РОДОМ</h1>
-        <YMaps>
-          <div>
-            <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
-          </div>
-        </YMaps>
-      </Jumbotron>
+    <Container className="p-3" fluid>
+      <h1 style={{ textAlign: 'center' }} className="m-5">НАЙДИ ОТКУДА ТЫ РОДОМ</h1>
+      <YMaps style={{ width: '100%' }}>
+        <Map
+          defaultState={{
+            center: [55.75, 37.57],
+            zoom: 9,
+            controls: ['zoomControl', 'fullscreenControl'],
+          }}
+          modules={['control.ZoomControl', 'control.FullscreenControl']}
+        >
+          <Placemark defaultGeometry={[55.75, 37.57]} />
+        </Map>
+      </YMaps>
     </Container>
   )
 }
