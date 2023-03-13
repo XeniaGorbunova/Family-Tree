260bb93fdc6fcfb8bd56d32cb71a0542cd8b0d5f
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-03-13 16:15:20 +0300

    page styled

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,16 @@
+import './PageNotFound.css'
+
 function PageNotFound() {
-  return <h1>PageNotFound</h1>
+  return (
+    <div className="page__container mx-auto">
+      <div className="title">
+        <h1 className="text-uppercase">Такая страница не существует</h1>
+      </div>
+      <div className="image">
+        <h2 className="info">404</h2>
+      </div>
+    </div>
+  )
 }
 
 export default PageNotFound
